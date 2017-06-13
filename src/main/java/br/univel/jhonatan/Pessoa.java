4c0abdf14b9e1adbde82669e172fc4776cb35331
jmattana <jhonatan_mattana@outlook.com>
2017-06-12 21:20:34 -0300

    Criação do projeto.

---
+++ src/main/java/br/univel/jhonatan/Pessoa.java
@@ -0,0 +1,43 @@
+package br.univel.jhonatan;
+
+public class Pessoa {
+	private long id;
+	private int idade;
+	private String telefone;
+	private String nome;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
+
+/*
+  		<dependency>
+			<groupId>net.sf.jasperreports</groupId>
+			<artifactId>jasperreports</artifactId>
+			<version>6.3.1</version>
+		</dependency>
+
+
+ */
